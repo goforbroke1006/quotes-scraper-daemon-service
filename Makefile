04f93bcc352e881b596c2111d38b3a7c707aa571
goforbroke <go.for.broke1006@gmail.com>
2019-10-10 22:51:24 +0300

    oanda finished

---
+++ Makefile
@@ -0,0 +1,2 @@
+build/images:
+	docker build -f ./deployment/nodejs-xvfb/Dockerfile -t goforbroke1006/nodejs-xvfb .
--- src/command/oanda/index.js
+++ src/command/oanda/index.js
@@ -1,5 +1,72 @@
 import '@babel/polyfill'
 
-import { oanda } from "./../../service/oanda"
+import Nightmare from 'nightmare';
+import * as cheerio from "cheerio"
 
-oanda()
+let extractValue = cellEl => {
+    const str = cellEl.find('.inline_int').text() + cellEl.find('.pip').text()
+    return parseFloat(str)
+}
+
+let parseData = html => {
+    let result = [];
+    const $ = cheerio.load(html)
+    $('.inline_rates_container').each((ci, containerEl) => {
+        if (result.length > 4) 
+            return false;
+
+        $(containerEl).find('.rate_row').each((rri, rowEl) => {
+
+            const code = $(rowEl).find('.inline.title.left:nth-child(1)').text().trim()
+
+            const bidCell = $(rowEl).find('.inline.value.right:nth-child(2)')
+            const askCell = $(rowEl).find('.inline.value.right:nth-child(3)')
+
+            result.push({
+                code: code,
+                ask: extractValue($(askCell)),
+                bid: extractValue($(bidCell)),
+            })
+
+        })
+    })
+    return result
+}
+
+const nightmare = Nightmare({
+    show: false,
+    // waitTimeout: 1000, // in ms
+    // gotoTimeout: 1000, // in ms
+    // loadTimeout: 1000,
+    // executionTimeout: 1000,
+    // pollInterval: 50, //in ms
+})
+
+console.log("oanda init")
+
+nightmare
+    .goto('https://www1.oanda.com/lang/ru/currency/live-exchange-rates/')
+    .then(() => {
+
+        console.log("oanda loop")
+
+        setInterval(() => {
+
+            console.log('--------------------------------------------------')
+            nightmare
+                .evaluate(() => {
+                    return document.querySelector('#core_content').innerHTML;
+                })
+                .then((responseHtml) => {
+                    console.log(parseData(responseHtml))
+                })
+                .catch(err => { console.error(err) })
+
+        }, 500)
+
+    })
+    .catch((err) => {
+        console.error(err)
+    })
+
+console.log("oanda stop")
